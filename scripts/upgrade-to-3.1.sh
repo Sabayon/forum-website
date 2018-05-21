d461521919aa8b436221593d4193b228510381e7
André Jaenisch <andre.jaenisch@posteo.de>
2018-05-21 17:16:31 +0200

    Made upgrade-to-3.1 script more robust.

--- scripts/upgrade-to-3.1.sh
+++ scripts/upgrade-to-3.1.sh
@@ -1,25 +1,28 @@
 #!/bin/sh
-# Upgrades existing forum to latest version
+# Upgrades existing forum to latest version of 3.1 line
 
-DOWNLOAD_URL="https://download.phpbb.com/pub/release/3.1/3.1.4/phpBB-3.0.14_to_3.1.4.zip"
+ZIP_FILE="phpBB-3.0.14_to_3.1.12.zip"
+DOWNLOAD_URL="https://download.phpbb.com/pub/release/3.1/3.1.12/$ZIP_FILE"
 PHP_ROOT_DIR="/var/www/html/phpBB3"
 
 cd /tmp
 rm -rf phpBB*
 
 wget -q "$DOWNLOAD_URL"
-unzip -q -o phpBB-3.0.14_to_3.1.12.zip -d phpBB3
+unzip -q -o "$ZIP_FILE" -d phpBB3
 
 rm -rf "$PHP_ROOT_DIR/vendor/"
 rm -rf "$PHP_ROOT_DIR/cache/"
+rm -rf "$PHP_ROOT_DIR/mod/"
+rm -rf "$PHP_ROOT_DIR/to-reinstall"
 
-mkdir "$PHP_ROOT_DIR/to-reinstall"
+mkdir -p "$PHP_ROOT_DIR/to-reinstall"
 mv "$PHP_ROOT_DIR/language/" "$PHP_ROOT_DIR/to-reinstall/"
 mv "$PHP_ROOT_DIR/styles/" "$PHP_ROOT_DIR/to-reinstall/"
 
 mkdir -p "$PHP_ROOT_DIR/language/en/"
 mkdir -p "$PHP_ROOT_DIR/styles/"
-mkdir "$PHP_ROOT_DIR/cache/"
+mkdir -p "$PHP_ROOT_DIR/cache/"
 chmod 777 "$PHP_ROOT_DIR/cache/"
 
 mv "$PHP_ROOT_DIR/to-reinstall/language/en/" "$PHP_ROOT_DIR/language/"
