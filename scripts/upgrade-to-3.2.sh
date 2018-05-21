c96085241fc2d2c326e4470f9f92f1e209c6170a
André Jaenisch <andre.jaenisch@posteo.de>
2018-05-21 17:31:41 +0200

    Moved logic regarding upgrade to a shared shell script.

--- scripts/upgrade-to-3.2.sh
+++ scripts/upgrade-to-3.2.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
-# Upgrades existing forum to latest version
+# Upgrades existing forum to latest version of 3.2 line
 
+CWD=`pwd`
 DOWNLOAD_URL="https://www.phpbb.com/files/release/phpBB-3.2.2.zip"
 
-cd /tmp
-rm -rf phpBB*
-
-wget -q "$DOWNLOAD_URL"
-unzip -q -o phpBB-3.2.2.zip
-
-rm phpBB3/config.php
-rm -r phpBB3/images/
-rm -r phpBB3/store/
-rm -r phpBB3/files/
-
-rm -rf /var/www/html/phpBB3/vendor/
-rm -rf /var/www/html/phpBB3/cache/
-
-cp -rf phpBB3 /var/www/html/phpBB3/
+sh -c "$CWD/scripts/upgrade-forum.sh $DOWNLOAD_URL"
