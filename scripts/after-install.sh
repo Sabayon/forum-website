28f44f01d22f72435dd3f4f9366c25f7ffcddc95
André Jaenisch <andre.jaenisch@posteo.de>
2018-05-21 17:15:48 +0200

    Fix after-install shell script.

--- scripts/after-install.sh
+++ scripts/after-install.sh
@@ -2,9 +2,10 @@
 # Moves the installation directory after setup so the forum becomes available
 
 SABAYON_FORUM=/tmp/html
+PHP_ROOT_DIR="/var/www/html/phpBB3"
 
 # FIXME: Needs to be run on the VM by user php
-cd /var/www/html/phpBB3
+cd "$PHP_ROOT_DIR"
 mkdir -p old
 mv install/ old/
 
@@ -14,7 +15,8 @@ cp $SABAYON_FORUM/data_global.php .
 cp $SABAYON_FORUM/favicon.ico .
 cp $SABAYON_FORUM/functions.php .
 cp $SABAYON_FORUM/get_langpacks.sh .
-cp -r $SABAYON_FORUM/mods/ .
 cp -r $SABAYON_FORUM/images/smilies/*.gif ./images/smilies/
-cp -r $SABAYON_FORUM/language/* .
-cp -r $SABAYON_FORUM/styles/* .
+cp -r $SABAYON_FORUM/language/* ./language/
+cp -r $SABAYON_FORUM/styles/* ./styles/
+
+chmod 640 "$PHP_ROOT_DIR/config.php"
