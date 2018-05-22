3bc53bf1804f9d08d0e1da85acaa4601df57f327
André Jaenisch <andre.jaenisch@posteo.de>
2018-05-22 22:06:28 +0200

    Fixed inaccuracies.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,11 +5,11 @@ apt-get -qq update
 apt-get -qq install -y apache2 php postgresql postgresql-client unzip libapache2-mod-php7.0 php-xml
 
 HTML_DOCUMENT_ROOT="/var/www/html"
-PHPBB_ROOT_PATH="$HTML_DOCUMENT_ROOT/phpbb3"
+PHPBB_ROOT_PATH="$HTML_DOCUMENT_ROOT/phpBB3"
 
 DOWNLOAD_URL="https://download.phpbb.com/pub/release/3.0/3.0.14/phpBB-3.0.14.zip"
 ZIP_FILE="`basename $DOWNLOAD_URL`"
-INSTALLATION_PATH="/tmp/"
+INSTALLATION_PATH="/tmp"
 
 # Download into a clean directory
 cd "$INSTALLATION_PATH"
@@ -21,15 +21,17 @@ echo "Download complete. Copying files to target destination."
 unzip -q -o "$ZIP_FILE" -d "$INSTALLATION_PATH"
 
 cp -r "$INSTALLATION_PATH/phpBB3" "$HTML_DOCUMENT_ROOT"
+# This should work, but fails for some reason
+#id -u php &> /dev/null || adduser --disabled-password --gecos "" php
+adduser --disabled-password --gecos "" php
 chmod 666 "$PHPBB_ROOT_PATH/config.php"
 chmod 777 "$PHPBB_ROOT_PATH/cache/"
 chmod 777 "$PHPBB_ROOT_PATH/files/"
 chmod 777 "$PHPBB_ROOT_PATH/store/"
 chmod 777 "$PHPBB_ROOT_PATH/images/avatars/upload/"
-chown -R php:root "$PHPBB_ROOT_PATH/"
+chown -R php "$PHPBB_ROOT_PATH/"
 
 echo "Files copied. Preparing database."
-id -u php &> /dev/null || adduser --disabled-password --gecos "" php
 sed -i.bak s/peer/trust/g /etc/postgresql/9.5/main/pg_hba.conf
 sudo -u postgres dropdb --if-exists forum
 sudo -u postgres dropuser php
